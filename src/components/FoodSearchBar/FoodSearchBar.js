835297d02f0c27bb69db80d672e2d1996ffd26d2
Anthony D <dunahee@gmail.com>
2019-02-11 14:48:24 -0600

    refactoring and removing unused code after connecting to rs

--- src/components/FoodSearchBar/FoodSearchBar.js
+++ src/components/FoodSearchBar/FoodSearchBar.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import Autosuggest from 'react-autosuggest';
 import { connect } from 'react-redux';
 
-
-
 // tells autosuggest how to read suggestions for user from redux store
 const getSuggestionValue = suggestion => suggestion.name;
 
@@ -15,32 +13,14 @@ const renderSuggestion = suggestion => (
 );
 
 class FoodSearchBar extends Component {
-    constructor() {
-        super();
-        // Autosuggest is a controlled component.
-        // This means that you need to provide an input value
+    constructor(props) {
+        super(props);
+        // Autosuggest is a controlled component that requires an input value
         // and an onChange handler that updates this value (see below).
         this.state = {
             value: '',
         };
     }
-
-    // Teach Autosuggest how to calculate suggestions for any given input value.
-    getSuggestions = value => {
-        //removes white space and capitals
-        const inputValue = value.trim().toLowerCase();
-        const inputLength = inputValue.length;
-
-        // Imagine you have a list of languages that you'd like to autosuggest.
-        let foodResults = [];
-        if (inputLength > 1) {
-            this.props.dispatch({ type: 'FETCH_FOODS', payload: inputValue })
-            return foodResults;
-        } else {
-            return foodResults
-        }
-
-    };
 
     onChange = (event, { newValue }) => {
         this.setState({
@@ -48,63 +28,72 @@ class FoodSearchBar extends Component {
         });
     };
 
-    // Autosuggest will call this function every time you need to update suggestions.
-    // You already implemented this logic above, so just use it.
-    onSuggestionsFetchRequested = ({ value }) => {
-        this.setState({
-            suggestions: this.getSuggestions(value)
-        });
-    };
-
-    // Autosuggest will call this function every time you need to clear suggestions.
-    onSuggestionsClearRequested = () => {
-        this.props.dispatch({type: 'CLEAR_SUGGESTIONS'});
-    };
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state.value);
-        this.props.dispatch({type: 'ADD_FOOD_TO_PANTRY', payload: this.state.value})
+    // Teach Autosuggest how to calculate suggestions for any given input value.
+    getSuggestions = value => {
+        //removes white space and capitals
+        const inputValue = value.trim().toLowerCase();
+        const inputLength = inputValue.length;
+        if (inputLength > 2) {
+            this.props.dispatch({ type: 'FETCH_FOODS', payload: inputValue })
+        };
     }
 
-    render() {
-        const value = this.state.value;
-         // Suggestions also need to be provided to the Autosuggest,
-        // and they are initially empty because the Autosuggest is closed.
-        const suggestions = this.props.suggestions.map(food => {
-        return { name: food } });
-        // Autosuggest will pass through all these props to the input.
-        const inputProps = {
-            placeholder: `eg 'ba'`,
-            value,
-            onChange: this.onChange
+        // Autosuggest will call this function every time you need to update suggestions.
+        // You already implemented this logic above, so just use it.
+        onSuggestionsFetchRequested = ({ value }) => {
+            this.getSuggestions(value)
         };
 
-        console.log(this.state);
-        // Finally, render it!
-        return (
+        // Autosuggest will call this function every time you need to clear suggestions.
+        onSuggestionsClearRequested = () => {
+            this.props.dispatch({ type: 'CLEAR_SUGGESTIONS' });
+        };
 
-            <form onSubmit={this.handleSubmit}>
-            <p>Type two letters to use the autocompleting search bar to quickly add foods in your pantry.</p>
-            <label htmlFor='foods-search-bar'>Search Bar for Foods</label>
-                <Autosuggest
-                    suggestions={suggestions}
-                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                    getSuggestionValue={getSuggestionValue}
-                    renderSuggestion={renderSuggestion}
-                    inputProps={inputProps}
-                    id='foods-search-bar'
-                />
-                <button type="submit">Add To Pantry</button>
-            </form>
+        handleSubmit = (event) => {
+            event.preventDefault();
+            console.log(this.state.value);
+            this.props.dispatch({ type: 'ADD_FOOD_TO_PANTRY', payload: this.state.value })
+        }
 
-        );
+        render() {
+            const value = this.state.value;
+            // Suggestions also need to be provided to the Autosuggest,
+            // and they are initially empty because the Autosuggest is closed.
+            const suggestions = this.props.suggestions.map(food => {
+                return { name: food }
+            });
+            // Autosuggest will pass through all these props to the input.
+            const inputProps = {
+                placeholder: `eg 'bac'`,
+                value,
+                onChange: this.onChange
+            };
+
+            console.log(this.state);
+            // Finally, render it!
+            return (
+
+                <form onSubmit={this.handleSubmit}>
+                    <p>Type two letters to use the autocompleting search bar to quickly add foods in your pantry.</p>
+                    <label htmlFor='foods-search-bar'>Search Bar for Foods</label>
+                    <Autosuggest
+                        suggestions={suggestions}
+                        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                        getSuggestionValue={getSuggestionValue}
+                        renderSuggestion={renderSuggestion}
+                        inputProps={inputProps}
+                        id='foods-search-bar'
+                    />
+                    <button type="submit">Add To Pantry</button>
+                </form>
+
+            );
+        }
     }
-}
 
-const mapRStoProps = (rs) => {
-    return ({ suggestions: rs.food.searchResults })
-}
+    const mapRStoProps = (rs) => {
+        return ({ suggestions: rs.food.searchResults })
+    }
 
-export default connect(mapRStoProps)(FoodSearchBar);
+    export default connect(mapRStoProps)(FoodSearchBar);
