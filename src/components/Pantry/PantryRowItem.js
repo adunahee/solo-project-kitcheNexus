8b775ecce5b5cd229a2841b1fcfa1904a15d2714
Anthony D <dunahee@gmail.com>
2019-02-15 17:20:06 -0600

    pantry table conditionally renders for selecting foods for batch items

--- src/components/Pantry/PantryRowItem.js
+++ src/components/Pantry/PantryRowItem.js
@@ -10,7 +10,7 @@ class PantryRowItem extends Component {
             let confirmMore = await window.confirm('Do you want to order more?');
             this.props.dispatch({ type: 'REMOVE_FROM_PANTRY', payload: this.props.foodObj.pantry_id });
             if (confirmMore) {
-                this.props.dispatch({ type: 'ADD_FOOD_TO_GROCERY', payload: {groceries: [this.props.foodObj.food_name]} });
+                this.props.dispatch({ type: 'ADD_FOOD_TO_GROCERY', payload: { groceries: [this.props.foodObj.food_name] } });
             }
         } else {
             alert('Removal canceled.');
@@ -18,21 +18,29 @@ class PantryRowItem extends Component {
 
     }
 
+    handleCheckbox = () => {
+        console.log('box checked with this action:', this.props.action);
+        //add in logic to dispatch to batch action reducer to store object for corresponding action(post to grocery or delete from pantry)
+        //pantry delete likely needs to be updated to handle multiple deletes
+    }
+
     render() {
         return (
-            <tr onClick={this.handleClick}>
-            <td>
-                checkbox
-            </td>
+            <tr onClick={ this.props.action === '' ? this.handleClick : null}>
                 <td>
                     {this.props.foodObj.food_name}
                 </td>
                 <td>
                     {this.props.foodObj.pantry_tag_name}
-        </td>
+                </td>
                 <td>
                     othershit
-        </td>
+                </td>
+                <td>
+                    {this.props.action === '' ? 
+                        null : 
+                        <input type='checkbox' onChange={this.handleCheckbox}/>}
+                </td>
             </tr>
         )
     }
--- src/components/Pantry/PantryTable.js
+++ src/components/Pantry/PantryTable.js
@@ -5,32 +5,72 @@ import PantryRowItem from './PantryRowItem';
 
 
 class PantryTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            action: '',
+
+        }
+    }
 
     buildPantryTableRows = () => {
         return this.props.pantry.map((foodObj, i) => {
-            return <PantryRowItem key={i} foodObj={foodObj} />
+            return <PantryRowItem key={i} foodObj={foodObj} action={this.state.action} />
         })
     }
 
+    handleActionChange = (event) => {
+        this.setState({ action: event.target.value });
+    }
+
     render() {
+        let actionButtonDescription;
+        switch (this.state.action) {
+            case ('Buy More'):
+                actionButtonDescription = 'Add To Groceries';
+                break;
+            case ('Used Up'):
+                actionButtonDescription = 'Remove';
+                break;
+            case ('Find Recipes'):
+                actionButtonDescription = 'Begin Search';
+                break;
+            default:
+                actionButtonDescription = '';
+        }
+
+
         return (
             <div>
                 <h2>Your Pantry</h2>
+                <select value={this.state.action} onChange={this.handleActionChange}>
+                    <option value=''>
+                        Many Item Update?
+                    </option>
+                    <option value='Buy More'>
+                        Buy More
+                    </option>
+                    <option value='Used Up'>
+                        Used Up
+                    </option>
+                    <option value='Find Recipes'>
+                        Find Recipes
+                    </option>
+                </select>
+                {this.state.action !== null && <button>{actionButtonDescription}</button>}
                 <table>
                     <thead>
                         <tr>
                             <th>
-
+                                Food Name
                             </th>
                             <th>
-                            Food Name
+                                Pantry Tag
                             </th>
                             <th>
-                            Pantry Tag
+                                Date Added
                             </th>
-                            <th>
-                            Date Added
-                            </th>
+                            <th>{this.state.action === '' ? null : 'Select'}</th>
                         </tr>
                     </thead>
                     <tbody>
