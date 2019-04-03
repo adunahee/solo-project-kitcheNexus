a28c48b342f1a3f29239be65c61ef788b34517e6
Anthony D <dunahee@gmail.com>
2019-04-03 14:03:26 -0500

    build works and added unit testing for foodreducer recentRecipes

---
+++ build/precache-manifest.be1b5e0b827018534ffc81922e1786e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "89c16e62d26e1c250d4e",
+    "url": "/static/css/main.32eb2a73.chunk.css"
+  },
+  {
+    "revision": "89c16e62d26e1c250d4e",
+    "url": "/static/js/main.dfdc9506.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "badeb66d3b39d5c6c458",
+    "url": "/static/js/2.766fca34.chunk.js"
+  },
+  {
+    "revision": "d64049e7a1103c9eb348aaaa4c80402b",
+    "url": "/index.html"
+  }
+];
--- src/test/foodReducer.test.js
+++ src/test/foodReducer.test.js
@@ -1,8 +1,11 @@
 import * as foodReducer from '../redux/reducers/foodReducer';
 
-const searchResults = foodReducer.searchResults;
+//destructuring foodReducers
+const { searchResults, foodSearchValue,
+    recipeHits, recipeSearchValue,
+    recentRecipes, recipeFavorites } = foodReducer;
 //tests for searchResults reducer
-test('should return default state of empty array', ()=>{
+test('searchResults: default return state of empty array', () => {
     const action = {};
     const defaultState = searchResults(undefined, action);
     expect(Array.isArray(defaultState)).toBe(true);
@@ -10,22 +13,60 @@ test('should return default state of empty array', ()=>{
 })
 
 test('SET_FOOD_SEARCH should receive an array and set state to it', () => {
-    const action ={type: 'SET_FOOD_SEARCH', payload: ['taco', 'burrito', 'other server data']}
+    const action = { type: 'SET_FOOD_SEARCH', payload: ['taco', 'burrito', 'other server data'] }
     expect(searchResults(undefined, action)).toEqual(['taco', 'burrito', 'other server data']);
 })
 
-test('search results should clear on CLEAR_SUGGESTION', ()=> {
-    const action = {type: 'CLEAR_SUGGESTIONS'}
+test('search results should clear on CLEAR_SUGGESTION', () => {
+    const action = { type: 'CLEAR_SUGGESTIONS' }
     const clearedState = searchResults(undefined, action);
     expect(Array.isArray(clearedState)).toBe(true);
     expect(clearedState.length).toBe(0);
 })
 
-test('ignores actions', ()=> {
-    const action = {type: 'SE_OMELE_DU_FROMAGE', payload: ['hotcarl']}
+test('ignores actions', () => {
+    const action = { type: 'SE_OMELE_DU_FROMAGE', payload: ['hotcarl'] }
     const ignoredState = searchResults(['a non zero array length'], action);
     expect(Array.isArray(ignoredState)).toBe(true);
     expect(ignoredState.length).toBe(1);
 })
 
-//other food reducer tests
+//recentRecipes reducer tests
+test('default state should be an empty array', () => {
+    const action = {};
+    const defaultState = recentRecipes(undefined, action);
+    expect(Array.isArray(defaultState)).toBe(true);
+    expect(defaultState.length).toBe(0);
+});
+
+test('SET_RECENT_RECIPES should be set a new state equal to users recent recipes', () => {
+    const action = {
+        type: 'SET_RECENT_RECIPES', 
+        payload: [
+            {key: 'recipe one Name'}, 
+            {key: 'recipe two Name'}, 
+            {key: 'recipe three Name'}
+        ]
+    };
+    const recentRecipeState = recentRecipes([], action);
+    expect(Array.isArray(recentRecipeState)).toBe(true);
+    expect(recentRecipeState.length).toBe(3);
+    expect(recentRecipeState).toEqual([
+        { key: 'recipe one Name' },
+        { key: 'recipe two Name' },
+        { key: 'recipe three Name' }
+    ]);
+});
+
+test('recentRecipes should ignore actions by default and return default state', () => {
+    const action = {type: 'PASTA_MASTACCIOLI', payload: [{key: "a cool new object"}]};
+    const currentState = [
+        { key: 'recipe one Name' },
+        { key: 'recipe two Name' },
+        { key: 'recipe three Name' }
+    ];
+    const ignoredActionState = recentRecipes(currentState, action);
+    expect(ignoredActionState.length).toBe(3);
+    expect(Array.isArray(ignoredActionState)).toBe(true);
+    expect(ignoredActionState).toEqual(currentState);
+});
